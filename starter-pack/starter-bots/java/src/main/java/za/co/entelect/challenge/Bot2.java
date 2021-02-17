efbbe3665204c7d4d5fe4264cd63602181698054
Govindabhakta <igovindabhakta@gmail.com>
2021-02-17 13:33:33 +0700

    Added Bot2 Alternative

---
+++ starter-pack/starter-bots/java/src/main/java/za/co/entelect/challenge/Bot2.java
@@ -0,0 +1,34 @@
+package za.co.entelect.challenge;
+
+import javafx.geometry.Pos;
+import za.co.entelect.challenge.command.*;
+import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.CellType;
+import za.co.entelect.challenge.enums.Direction;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Bot2 {
+
+    /* Contains all possible moves */
+    /* Responsible for calling commands */
+    class MoveSet {
+
+
+    }
+
+    /* Contains all possible actions and their values */
+    /* Responsible for evaluating board and selecting best current action */
+    class ActionPlanner {
+
+    }
+
+    private double getEuclideanDistance(int x, int y)
+    {
+        return 0.0;
+    }
+
+}
+
+
